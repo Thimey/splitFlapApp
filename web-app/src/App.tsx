6326e762d556b1a1c51c5e59ab95b0f557093736
Simon Angell <simon.angell307@gmail.com>
2021-04-10 22:05:48 +1000

    Add splitflap input

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,15 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+import { createUseStyles } from 'react-jss'
+
 import { publishDisplays } from './services/publishDisplays'
 import { publishReset } from './services/publishReset'
 import { publishEnableMotors } from './services/publishEnableMotors'
 import { publishDisableMotors } from './services/publishDisableMotors'
 import './App.css';
+import { FlapInput } from './FlapInput'
+
+const NUMBER_OF_SPLIT_FLAPS = 8
+
+const useStyles = createUseStyles({
+  splitFlapDisplay: {
+    paddingTop: 8,
+    paddingBottom: 8,
+    display: 'flex',
+    justifyContent: 'center',
+    cursor: 'text',
+    '& > *': {
+      marginLeft: 4,
+      boxShadow: 'rgb(0 0 0 / 20%) 0px 3px 3px -2px, rgb(0 0 0 / 14%) 0px 3px 4px 0px, rgb(0 0 0 / 12%) 0px 1px 8px 0px'
+    }
+  },
+  hide: {
+    opacity: 0,
+    position: 'absolute'
+  },
+  flapsFocused: {
+    border: "1px solid"
+  }
+})
 
 function App() {
+  const classes = useStyles()
   const [displayInput, setDisplayInput] = useState('');
   const [delay, setDelay] = useState(500);
   const [displays, setDisplays] = useState<string[]>([])
   const [publishImmediately, setPublishImmediately] = useState(false);
+  const [displayInputFocused, setDisplayInputFocused] = useState(false)
+
+  const inputRef = React.useRef<HTMLTextAreaElement | null>(null)
 
   const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => setDisplayInput(e.target.value);
   const handleDelayChange = (e: React.ChangeEvent<HTMLInputElement>) => setDelay(Number(e.target.value));
@@ -32,10 +62,6 @@ function App() {
     setDisplayInput('');
   }
 
-  const handleReset = () => publishReset();
-  const handleDisableMotors = () => publishDisableMotors();
-  const handleEnableMotors = () => publishEnableMotors();
-
   const handleInputKeyUp = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
     if (e.key === 'Enter') {
       handleOnInputEnter();
@@ -44,11 +70,34 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={handleReset}>Reset</button>
-      <button onClick={handleDisableMotors}>Disable Motors</button>
-      <button onClick={handleEnableMotors}>Enable Motors</button>
+      <div
+        className={classes.splitFlapDisplay}
+        onClick={() => inputRef.current?.focus()}
+      >
+        {
+          Array(NUMBER_OF_SPLIT_FLAPS).fill(null).map((_, i) => (
+            <FlapInput
+              character={displayInput[i]}
+              blink={displayInputFocused && displayInput.length === i}
+            />
+          ))
+        }
+      </div>
+
+      <textarea
+        ref={inputRef}
+        className={classes.hide}
+        onFocus={() => setDisplayInputFocused(true)}
+        onBlur={() => setDisplayInputFocused(false)}
+        onKeyUp={handleInputKeyUp}
+        onChange={handleInputChange}
+        value={displayInput}
+      />
+
+      <button onClick={() => publishReset()}>Reset</button>
+      <button onClick={() => publishDisableMotors()}>Disable Motors</button>
+      <button onClick={() => publishEnableMotors()}>Enable Motors</button>
       <input type="number" onChange={handleDelayChange} value={delay} />
-      <textarea onKeyUp={handleInputKeyUp} onChange={handleInputChange} value={displayInput} />
       <input id='publish' type='checkbox' onChange={handlePublishOptionChange} checked={publishImmediately} />
       <label htmlFor='publish' >Publish on Enter</label>
 
---
+++ web-app/src/FlapInput.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import classnames from 'classnames'
+import { createUseStyles } from 'react-jss'
+
+const useStyles = createUseStyles({
+    caseFrame: {
+        border: '1px solid black',
+        height: '100px',
+        width: '50px',
+    },
+    noBlink: {
+        fill: 'white',
+    },
+    '@keyframes blinker': {
+        from: { fill: 'black' },
+        to: { fill: 'white' }
+    },
+    blink: {
+        fill: 'black',
+        animationName: '$blinker',
+        animationTimingFunction: 'unset',
+        animationDuration: '0.530s',
+        animationIterationCount: 'infinite',
+    },
+})
+
+const scale = 0.5
+
+const WIDTH = 100 * scale
+const HEIGHT = 150 * scale
+const X = 0
+const Y = 0
+
+const BORDER_X = 0.1 * WIDTH
+const BORDER_TOP = 0.1 * HEIGHT
+const BORDER_BOTTOM = 0.07 * HEIGHT
+const FLAP_BORDER = 0.04 * WIDTH
+const FLAP_GAP = 0.03 * WIDTH
+const FLAP_WIDTH = WIDTH - 2 * (BORDER_X + FLAP_BORDER)
+const FLAP_HEIGHT = (HEIGHT - FLAP_GAP - (2 * FLAP_BORDER) - BORDER_TOP - BORDER_BOTTOM) / 2
+
+export interface Props {
+    character: string
+    blink: boolean
+}
+
+export const FlapInput: React.FC<Props> = ({ character, blink }) => {
+    const classes = useStyles()
+
+    return (
+        <svg
+            viewBox={`${X} ${Y} ${WIDTH} ${HEIGHT}`}
+            width={WIDTH}
+            height={HEIGHT}
+        >
+            {/* Outer box */}
+            <rect
+                x={X}
+                y={Y}
+                width={WIDTH}
+                height={HEIGHT}
+                style={{ fill: "rgb(11, 11, 11)" }}
+            />
+            {/* Inner box */}
+            <rect
+                x={X + BORDER_X}
+                y={Y + BORDER_TOP}
+                width={WIDTH - (2 * BORDER_X)}
+                height={HEIGHT - BORDER_TOP - BORDER_BOTTOM}
+                className={classnames({
+                    [classes.noBlink]: !blink,
+                    [classes.blink]: blink,
+                })}
+            // style={{ fill: focused ? '#e3e2df' : 'rgb(245, 245, 245)' }}
+            />
+            {/* Top flap */}
+            <rect
+                x={X + BORDER_X + FLAP_BORDER}
+                y={Y + BORDER_TOP + FLAP_BORDER}
+                width={FLAP_WIDTH}
+                height={FLAP_HEIGHT}
+            />
+            {/* Bottom flap */}
+            <rect
+                x={X + BORDER_X + FLAP_BORDER}
+                y={Y + BORDER_TOP + FLAP_BORDER + FLAP_HEIGHT + FLAP_GAP}
+                width={FLAP_WIDTH}
+                height={FLAP_HEIGHT}
+            />
+            {/* Character */}
+            <text
+                style={{
+                    fill: "rgb(254, 254, 254)",
+                    fontSize: `${0.9 * WIDTH}px`,
+                    whiteSpace: "pre"
+                }}
+                x={X + (WIDTH / 2)}
+                y={(Y + (HEIGHT / 2)) * 1.1}
+                textAnchor="middle"
+                dominantBaseline="middle"
+            >
+                {(character || '').toUpperCase()}
+            </text>
+        </svg>
+    )
+}
+
